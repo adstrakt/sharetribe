f9ef82e8265a1be250627e9a11b17ece57fad78a
Mikko Porkola <mikko.porkola@sharetribe.com>
2016-10-10 10:49:47 +0300

    Refactor ImageModel

--- client/app/models/ListingModel.js
+++ client/app/models/ListingModel.js
@@ -1,15 +1,16 @@
 import Immutable from 'immutable';
-import { Image, ImageRefs } from './ImageModel';
+import { Image, ResponsiveImage } from './ImageModel';
+import { Profile } from './ProfileModel';
 
 const ListingModel = Immutable.Record({
   id: 'uuid',
   title: 'Listing',
-  images: new Immutable.List([new ImageRefs({
-    square: new Image(),
-    square2x: new Image(),
+  images: new Immutable.List([new ResponsiveImage({
+    '1x': new Image(),
+    '2x': new Image(),
   })]),
   authorId: 'foo',
-  author: new Immutable.Record(),
+  author: new Profile(),
 
   // these need to be updated
   price: 1,
@@ -20,10 +21,15 @@ const ListingModel = Immutable.Record({
   listingURL: 'https://example.com/listing/1',
 });
 
+const parseListingImages = (images) => new ResponsiveImage({
+  '1x': images.square,
+  '2x': images.square2x,
+});
+
 export const parse = (l) => new ListingModel({
   id: l.get(':id'),
   title: l.getIn([':attributes', ':title']),
-  images: l.getIn([':attributes', ':images']),
+  images: l.getIn([':attributes', ':images']).map(parseListingImages),
   authorId: l.getIn([':relationships', ':author', ':id']),
 });
 
